c60689b7f553fb812eae44fe8526d75453dd97f9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-06-08 21:49:32 -0300

    vjwefwnr

---
+++ src/problem2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // O usuário começa na coluna 12, linha 1
+        int y = 1;
+        int x = 12;
+        double altmare = 0;
+        String velvento;
+
+
+
+
+
+
+    }
+}
